c7e34c920c0aec1b5acb06daab22be9efbcdcebd
Jesse H. (JesseLikesWeather) <134172009+JesseLikesWeatherakaYoshiKart@users.noreply.github.com>
2023-10-30 15:57:10 -0400

    Update config.js

--- other-scripts/FRESHWDL/config.js
+++ other-scripts/FRESHWDL/config.js
@@ -1,11 +1,11 @@
 /*jslint plusplus: true, sloppy: true, indent: 4 */
 
 //Ajust these values to your liking.
-var clientRawName = "clientraw.txt", //The names of your clientraw files
+var clientRawName = "https://www.halethorpeweather.com/clientraw.txt", //The names of your clientraw files
     clientRawExtraName = "clientrawextra.txt",
     clientRawHourName = "clientrawhour.txt",
     clientRawDailyName = "clientrawdaily.txt",
-    customBaseURL = false, // OPTIONAL: Set the path to where your clientraw files are uploaded e.g., "http://www.goldenbaynzweather.info/wdl/" (note: final backslash and quotation marks must be included). Otherwise leave as: false
+    customBaseURL = true, // OPTIONAL: Set the path to where your clientraw files are uploaded e.g., "http://www.goldenbaynzweather.info/wdl/" (note: final backslash and quotation marks must be included). Otherwise leave as: false
     lang = "en", //Set Language. To see what lanuages are currently supported, see the readme file at: https://github.com/Yerren/FreshWDL/blob/master/README.md
     currentUnits = { //Default units (what the page will display when first loaded)
         pressure: "inHg",        //Options: "hPa" "mmHG" "kPa" "inHg" "mb"
