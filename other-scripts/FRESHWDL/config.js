d12c5957228709e0ba60a21ebd34d35f45f840e7
Jesse H. (JesseLikesWeather) <134172009+JesseLikesWeatherakaYoshiKart@users.noreply.github.com>
2023-11-13 09:06:47 -0500

    Update config.js

--- other-scripts/FRESHWDL/config.js
+++ other-scripts/FRESHWDL/config.js
@@ -1,10 +1,10 @@
 /*jslint plusplus: true, sloppy: true, indent: 4 */
 
 //Ajust these values to your liking.
-var clientRawName = "http://yoshisweatherreport.rf.gd/clientraw.txt", //The names of your clientraw files
-    clientRawExtraName = "http://yoshisweatherreport.rf.gd/clientrawextra.txt",
-    clientRawHourName = "http://yoshisweatherreport.rf.gd/clientrawhour.txt",
-    clientRawDailyName = "http://yoshisweatherreport.rf.gd/clientrawdaily.txt",
+var clientRawName = "https://dinoswxreport.altervista.org/wp-content/clientraw.txt", //The names of your clientraw files
+    clientRawExtraName = "https://dinoswxreport.altervista.org/wp-content/clientrawextra.txt",
+    clientRawHourName = "https://dinoswxreport.altervista.org/wp-content/clientrawhour.txt",
+    clientRawDailyName = "https://dinoswxreport.altervista.org/wp-content/clientrawdaily.txt",
     customBaseURL = true, // OPTIONAL: Set the path to where your clientraw files are uploaded e.g., "http://www.goldenbaynzweather.info/wdl/" (note: final backslash and quotation marks must be included). Otherwise leave as: false
     lang = "en", //Set Language. To see what lanuages are currently supported, see the readme file at: https://github.com/Yerren/FreshWDL/blob/master/README.md
     currentUnits = { //Default units (what the page will display when first loaded)
