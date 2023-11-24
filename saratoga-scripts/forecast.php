ffae5317f2789dd16377b28d261af247424b6441
Jesse H. (JesseLikesWeather) <134172009+JesseLikesWeatherakaYoshiKart@users.noreply.github.com>
2023-11-24 09:11:57 -0500

    Add files via upload

---
+++ saratoga-scripts/forecast.php
@@ -0,0 +1,8 @@
+ <table>
+<tr align="center" valign="top">
+<?php print "<td>$forecasticons[0]</td><td>$forecasticons[1]</td>\n"; ?>
+</tr>
+<tr align="center" valign="top">
+<?php print "<td>$forecasttemp[0]</td><td>$forecasttemp[1]</td>\n"; ?>
+</tr>
+</table>
