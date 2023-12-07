cad8c94ac8e3e8dccf0b263e23cf9e87e14487ee
Jesse H. (JesseLikesWeather) <134172009+JesseLikesWeatherakaYoshiKart@users.noreply.github.com>
2023-12-07 09:43:20 -0500

    Create weather.py

---
+++ my-scripts/weather.py
@@ -0,0 +1,21 @@
+
+
+#This is a script I learned in a python course! I am excited! 12-7-23 9:19 AM, I wrote my first python code in the terminal.
+
+weather=input ("What is the current weather observation?")
+print ("You Observed,", weather)
+
+temp=input ("What is the current temperature?")
+print ("You Observed,", temp)
+
+location=input ("Your Location?")
+print ("Your Location is,", location)
+
+
+print ("Your Full Observation: The Location is", location, "and you observed..", weather, "and the temperature is", temp)
+
+print ("If you like this code, check out my other scripts on github! My Username is JesseLikesWeather :)")
+
+#I wrote the temperature input script just 4 minutes later.
+
+O
