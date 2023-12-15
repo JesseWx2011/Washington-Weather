1994b2042b3624235671bfd50db625e706599a9a
Jesse H. (JesseLikesWeather) <134172009+JesseLikesWeatherakaYoshiKart@users.noreply.github.com>
2023-12-14 19:21:46 -0500

    Update weather.py

--- my-scripts/weather.py
+++ my-scripts/weather.py
@@ -16,7 +16,7 @@ location=input ("Your Location?")
 print ("The Inputted Location is,", location)
 
 
-print ("Your Full Observation: The Location is", location, "and you observed..", weather, "and the temperature is", temp, "and the wind is", wind)
+print ("Your Full Observation: The Location is", location, "and you observed", weather, "and the temperature is", temp, "and the wind is", wind)
 
 print ("If you like this code, check out my other scripts on github! My Username is JesseLikesWeather :)")
 
@@ -25,4 +25,4 @@ print ("If you are in the United States, check the weather report for your area 
 
 #I wrote the temperature input script just 4 minutes later.
 
-#Last Update at 9:59 AM EDT 12/7/23 
+#Last Update at 7:21 PM EDT 12/14/23
